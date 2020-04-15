bea73de73828fd23d1ca7b2d8429bb698c2b3e44
amrukwa <a.mrukwa00@gmail.com>
2020-04-15 07:43:31 +0200

    Polish the structures

--- Source.c
+++ Source.c
@@ -4,17 +4,17 @@
 # include <math.h>
 # define max_iter 1000
 
-typedef struct {
+struct vectors {
 	int n_features;
 	int n_samples;
-	double * data; // 2D array containing n_samples vectors of n_features dimensions
-	int * labels; // 1D array containing label for each sample
-} data_space;
+	double* coords;
+};
 
-typedef struct {
-	int n_clusters;
-	double * centroids; // 2D array containing n_clusters vectors of n_features dimensions
-}cluster_params;
+struct dataspace {
+	struct vectors* data_space;
+	int* labels;
+};
+
 
 void check_if_data(char first, char second)
 {
@@ -41,7 +41,57 @@ void check_if_data(char first, char second)
 	}
 }
 
-void get_dimensions(data_space * dimensions, FILE * given_data)
+struct vectors* allocate_points(int* n_features, int* n_samples)
+{
+	struct vectors* points;
+	points = malloc(sizeof(struct vectors));
+	if (points == 0)
+	{
+		return 1;
+	}
+	points->n_features = *n_features;
+	points->n_samples = *n_samples;
+	points->coords = malloc(points->n_features * points->n_samples * sizeof(double));
+	if (points->coords == 0) {
+		free(points);
+		return 1;
+	}
+	return points;
+}
+
+void destroy_vectors(struct vectors* some_vectors)
+{
+	free(some_vectors->coords);
+	free(some_vectors);
+}
+
+struct dataspace* allocate_data(int* n_features, int* n_samples)
+{
+	struct dataspace* some_data;
+	some_data = malloc(sizeof(struct dataspace));
+	if (some_data == 0)
+	{
+		return 1;
+	}
+	some_data->data_space = allocate_points(n_features, n_samples);
+	some_data->labels = calloc(some_data->data_space->n_samples, sizeof(int));
+	if (some_data->labels == 0)
+	{
+		destroy_vectors(some_data->data_space);
+		free(some_data);
+		return 1;
+	}
+	return some_data;
+}
+
+void destroy_dataspace(struct dataspace* some_data)
+{
+	free(some_data->labels);
+	destroy_vectors(some_data->data_space);
+	free(some_data);
+}
+
+void get_dimensions(int* n_features, int* n_samples, FILE* given_data)
 {
 	int x = 0;
 	int y = 0;
@@ -52,64 +102,65 @@ void get_dimensions(data_space * dimensions, FILE * given_data)
 		{
 			x++;
 		}
-		else if(c == '\n')
+		else if (c == '\n')
 		{
 			x++;
 			y++;
 		}
-		
+
 		char b = c;
 		c = getc(given_data);
 		check_if_data(b, c);
 	}
-	dimensions->n_features = x / y;
-	dimensions->n_samples = y;
+	*n_features = x / y;
+	*n_samples = y;
 	fseek(given_data, 0L, SEEK_SET);
 }
 
-void get_clusters_n(cluster_params* some_clusters, data_space* dimensions)
+void get_clusters_n(int* clusters_number, int* n_samples)
 {
 	printf("Type the desired number of centroids.\n");
-	scanf("%i", &some_clusters->n_clusters);
-	if (some_clusters->n_clusters > dimensions->n_samples)
+	scanf("%i", clusters_number);
+	if (*clusters_number > * n_samples)
 	{
 		printf("You can't have more clusters than samples.\n");
 		exit(1);
 	}
 }
 
-void allocate_point_memory(data_space* dimensions, cluster_params* some_clusters) // add checking if memory was allocated
+void load_the_data(struct dataspace* taken_data, FILE* given_data)
 {
-	dimensions->data = (double*)malloc(dimensions->n_samples * dimensions->n_features * sizeof(double));
-	some_clusters->centroids = (double*)malloc(some_clusters->n_clusters * (dimensions->n_features) * sizeof(double));
-	dimensions->labels = (int*)calloc(dimensions->n_samples, sizeof(int)); // this way we can skip one iteration later on in first labeling and prevent reseting labels at each call
-}
-
-void load_the_data(data_space* dimensions, FILE* given_data)
-{
-	for (int y = 0; y < dimensions->n_samples; y++)
+	for (int y = 0; y < taken_data->data_space->n_samples; y++)
 	{
-		for (int x = 0; x < dimensions->n_features; x++)
+		for (int x = 0; x < taken_data->data_space->n_features; x++)
 		{
-			if (fscanf(given_data, "%lf", &dimensions->data[y * dimensions->n_features + x]) != 1)
+			if (fscanf(given_data, "%lf", &taken_data->data_space->coords[y * taken_data->data_space->n_features + x]) != 1)
 			{
 				printf("Check data file for typos.");
 				exit(1);
 			}
+
 		}
 	}
 }
 
-void print_to_check(cluster_params* some_clusters, data_space* dimensions)
+void print_to_check(struct vectors* some_clusters)
 {
-	for (int x = 0; x < some_clusters->n_clusters; x++)
+	for (int x = 0; x < some_clusters->n_samples; x++)
 	{
-		for (int y = 0; y < dimensions->n_features; y++)
+		for (int y = 0; y < some_clusters->n_features; y++)
 		{
-			printf("%lf", some_clusters->centroids[x * dimensions->n_features + y]);
-			printf(" ");
+			printf("%i: %lf ", x, some_clusters->coords[x * some_clusters->n_features + y]);
 		}
 		printf("\n");
+	}
+}
+
+void print_labels(struct dataspace* taken_data)
+{
+	for (int i = 0; i < taken_data->data_space->n_samples; i++)
+	{
+		printf("%i: %i ", i, taken_data->labels[i]);
 	}
 }
 
@@ -120,26 +171,6 @@ void swap(int* a, int* b)
 	*b = dummy;
 }
 
-void fisher_yates(int initial_dimensions, int desired_dimensions, int* indices);
-
-void initialize_k_means(data_space* dimensions, cluster_params* some_clusters)
-{
-	int* indices;
-	indices = (int*)malloc(sizeof(int) * dimensions->n_samples);
-	for (int i = 0; i < dimensions->n_samples; i++)
-	{
-		indices[i] = i;
-	}
-	fisher_yates(dimensions->n_samples, some_clusters->n_clusters, indices);
-	for (int j = 0; j < some_clusters->n_clusters; j++)
-	{
-		for (int k = 0; k < dimensions->n_features; k++)
-		{
-			some_clusters->centroids[j * dimensions->n_features + k] = dimensions->data[indices[j] * dimensions->n_features + k];
-		}
-	}	
-}
-
 void fisher_yates(int initial_dimensions, int desired_dimensions, int* indices)
 {
 	srand(time(NULL));
@@ -148,6 +179,25 @@ void fisher_yates(int initial_dimensions, int desired_dimensions, int* indices)
 		int a = rand() % initial_dimensions; // index between 1st and last data sample
 		swap(&indices[i], &indices[a]);
 	}
+}
+
+void initialize_k_means(struct dataspace* taken_data, struct vectors* some_clusters) // wtf
+{
+	int* indices;
+	indices = malloc(sizeof(int) * taken_data->data_space->n_samples);
+	for (int i = 0; i < taken_data->data_space->n_samples; i++)
+	{
+		indices[i] = i;
+	}
+	fisher_yates(taken_data->data_space->n_samples, some_clusters->n_samples, indices);
+	for (int j = 0; j < some_clusters->n_samples; j++)
+	{
+		for (int k = 0; k < some_clusters->n_features; k++) // here sth doesn't work
+		{
+			some_clusters->coords[j * some_clusters->n_features + k] = taken_data->data_space->coords[indices[j] * taken_data->data_space->n_features + k];
+		}
+	}
+	free(indices);
 }
 
 double Euclidean_distance(double* given_points, double* centres, int dimensions, int point_number, int centre_number) // calculates distance between two points
@@ -162,80 +212,90 @@ double Euclidean_distance(double* given_points, double* centres, int dimensions,
 	return distance;
 }
 
-void label_points(cluster_params* some_clusters, data_space* dimensions)
+void label_points(struct dataspace* taken_data, struct vectors* some_clusters)
 {
 	double cur_dist;
 	double min_dist;
 
-	for (int j = 0; j < dimensions->n_samples; j++)
+	for (int j = 0; j < taken_data->data_space->n_samples; j++)
 	{
-		min_dist = Euclidean_distance(dimensions->data, some_clusters->centroids, dimensions->n_features, j, 0);
-		dimensions->labels[j] = 0;
-		for (int k = 0; k < some_clusters->n_clusters; k++)
+		min_dist = Euclidean_distance(taken_data->data_space->coords, some_clusters->coords, taken_data->data_space->n_features, j, 0);
+		taken_data->labels[j] = 0;
+		for (int k = 0; k < some_clusters->n_samples; k++)
 		{
-			cur_dist = Euclidean_distance(dimensions->data, some_clusters->centroids, dimensions->n_features, j, k);
+			cur_dist = Euclidean_distance(taken_data->data_space->coords, some_clusters->coords, taken_data->data_space->n_features, j, k);
 			if (cur_dist < min_dist)
 			{
-				dimensions->labels[j] = k;
+				taken_data->labels[j] = k;
 				min_dist = cur_dist;
 			}
 		}
 	}
 }
 
-void calculate_centroids(cluster_params* some_clusters, data_space* dimensions) 
+void calculate_centroids(struct dataspace* taken_data, struct vectors* some_clusters)
 {
-	for (int cur_cent = 0; cur_cent < some_clusters->n_clusters; cur_cent++)
+	for (int cur_cent = 0; cur_cent < some_clusters->n_samples; cur_cent++)
 	{
-		for (int cur_dim = 0; cur_dim < dimensions->n_features; cur_dim++)
+		for (int cur_dim = 0; cur_dim < taken_data->data_space->n_features; cur_dim++)
 		{
 			double sum_on_axis = 0;
 			int members = 0;
-			for (int cur_point = 0; cur_point < dimensions->n_samples; cur_point++)
+			for (int cur_point = 0; cur_point < taken_data->data_space->n_samples; cur_point++)
 			{
-				if (dimensions->labels[cur_point] == cur_cent)
+				if (taken_data->labels[cur_point] == cur_cent)
 				{
-					members= members+1;
-					sum_on_axis = sum_on_axis + dimensions->data[cur_point * dimensions->n_features + cur_dim];
+					++members;
+					sum_on_axis += taken_data->data_space->coords[cur_point * taken_data->data_space->n_features + cur_dim];
 				}
-				some_clusters->centroids[cur_cent * dimensions->n_features + cur_dim] = sum_on_axis / members;
+				some_clusters->coords[cur_cent * some_clusters->n_features + cur_dim] = sum_on_axis / members;
 			}
 		}
 	}
 }
 
-void kmeans_algorithm(cluster_params* some_clusters, data_space* dimensions)
+void kmeans_algorithm(struct dataspace* taken_data, struct vectors* some_clusters)
 {
-	label_points(some_clusters, dimensions); // labeling first time for already initialized centroids
+	label_points(taken_data, some_clusters); // labeling first time for already initialized centroids
 	for (int i = 0; i < max_iter; i++)
 	{
-		calculate_centroids(some_clusters, dimensions); // recalculating centroids by means
-		label_points(some_clusters, dimensions); // labeling
+		calculate_centroids(taken_data, some_clusters); // recalculating centroids by means
+		label_points(taken_data, some_clusters); // labeling
 	}
 }
 
-void free_all(cluster_params* some_clusters, data_space* dimensions)
+void free_all(struct dataspace* taken_data, struct vectors* some_clusters)
 {
-	free(dimensions->data);
-	free(dimensions->labels);
-	free(some_clusters->centroids);
+	destroy_vectors(some_clusters);
+	destroy_dataspace(taken_data);
 }
 
 int main()
 {
-	data_space dimensions;
-	cluster_params some_clusters;
+	int n_features = 0;
+	int n_samples = 0;
+	int clusters_number = 0;
+	struct vectors* some_clusters;
+	struct dataspace* taken_data;
 	FILE* given_data;
+
 	fopen_s(&given_data, "data.txt", "r");
-	get_dimensions(&dimensions, given_data);
-	get_clusters_n(&some_clusters, &dimensions);
-	allocate_point_memory(&dimensions, &some_clusters);
-	load_the_data(&dimensions, given_data);
-    fclose(given_data);
-	initialize_k_means(&dimensions, &some_clusters);
-	print_to_check(&some_clusters, &dimensions);
-	kmeans_algorithm(&some_clusters, &dimensions);
-	print_to_check(&some_clusters, &dimensions); //remember to transform it into save_the_result
-	free_all(&some_clusters, &dimensions);
+	if (given_data == NULL)
+	{
+		perror("Opening the file failed.");
+		return 1;
+	}
+	get_dimensions(&n_features, &n_samples, given_data);
+	get_clusters_n(&clusters_number, &n_samples);
+	some_clusters = allocate_points(&n_features, &clusters_number);
+	taken_data = allocate_data(&n_features, &n_samples);
+	load_the_data(taken_data, given_data);
+	fclose(given_data);
+	initialize_k_means(taken_data, some_clusters);
+	print_to_check(some_clusters);
+	kmeans_algorithm(taken_data, some_clusters);
+	print_to_check(some_clusters); //remember to transform it into save_the_result
+	print_labels(taken_data);
+	free_all(taken_data, some_clusters);
 	return 0;
 }
